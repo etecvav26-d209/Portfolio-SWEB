fa42c3dbcbbdd87cc22001e8881c354aade3e257
Pedro Coraine <corainepedro3@gmail.com>
2026-08-08 21:35:44 -0300

    criando footer

---
+++ Bigodes/3-bimestre/criptografia-php/includes/footer.php
@@ -0,0 +1,18 @@
+<footer>
+
+    <div class="container">
+
+        <h2>Colaboradores</h2>
+
+        <p>Laura Duarte Arruda dos Santos</p>
+        <p>Nicolas Saraiva Batista</p>
+        <p>Pedro Coraine</p>
+        <p>Pedro Henrique Nascimento Rodrigues</p>
+
+    </div>
+
+</footer>
+
+</body>
+
+</html>
