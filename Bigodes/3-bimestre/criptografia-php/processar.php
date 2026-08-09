64bebeab0e4e3d62a87b45a375fa228f624ce163
Laura Duarte <dduartelaureta@gmail.com>
2026-08-09 00:02:17 -0300

    adicionando verificação do método sha256

--- Bigodes/3-bimestre/criptografia-php/processar.php
+++ Bigodes/3-bimestre/criptografia-php/processar.php
@@ -4,9 +4,15 @@ include("includes/header.php");
 
 $texto = $_POST["texto"];
 $metodo = $_POST["metodo"];
+
 if ($metodo == "md5") {
     $resultado = md5($texto);
 }
+
+if ($metodo == "sha256") {
+    $resultado = hash("sha256", $texto);
+}
+
 
 
 ?>
