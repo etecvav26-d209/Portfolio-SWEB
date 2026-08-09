ef36931a0f2aeba6fdee6406b0d9822b2b740bb3
Laura Duarte <dduartelaureta@gmail.com>
2026-08-08 23:49:25 -0300

    adicionando parte da exibição do resultado para exemplo

--- Bigodes/3-bimestre/criptografia-php/processar.php
+++ Bigodes/3-bimestre/criptografia-php/processar.php
@@ -7,6 +7,20 @@ $metodo = $_POST["metodo"];
 
 ?>
 
+<main>
+
+    <div class="container">
+
+        <h2>Resultado</h2>
+
+        <p>Texto informado: <?php echo $texto; ?></p>
+
+        <p>Método utilizado: <?php echo $metodo; ?></p>
+
+    </div>
+
+</main>
+
 <?php
 
 include("includes/footer.php");
